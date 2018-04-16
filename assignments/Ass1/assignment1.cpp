4f319ef5dfb043a611b94e8a4ff581f2e836ae6a
Jim Vargas <jimball1@hotmail.com>
2018-04-15 19:26:16 -0700

    assignment 1 commit: functionality appears good after simple testing

--- assignments/Ass1/assignment1.cpp
+++ assignments/Ass1/assignment1.cpp
@@ -1,10 +1,128 @@
 //Jim Vargas
-//This is my solution to CS162 Programming Assignment 1
+//CS162 Programming Assignment 1
+
+/*
+   This program will calculate the dimensions for a package given a package type and a fluid volume.
+   It begins by prompting for a volume amount in fluid ounces, then for a packaging type. Four total types are available:
+   'Cube,' 'Sphere,' 'Regular Cylinder,' 'Custom Cylinder.' Note that there are two 'cylinder' types.
+   The program first converts the input volume from fluid ounces to cubed inches, and then calculates the corresponding one dimensional dimensions from that.
+ */
 
 #include <iostream>
+#include <stdlib.h>
+#include <tgmath.h>
 using namespace std;
+
+
+
+//runner: runs the main functionality of the program. Contains all prompts, takes in all inputs, prints all outputs.
+//This is run on a loop in main(). Returns condition for the loop.
+int runner(void)
+{
+	/*
+	   finalSay is the 'final say' the user gets, for whether they want the program to terminate or not or complete another loop. 
+	   vol is inputted volume, convertDim is the converted volume. outputDim is a dimension that will be printed to the terminal.
+	   customRadius and cylinderHeight are additional variables for cylindrical calculations.
+	   inputChoice and inputChoice2 correspond to the user's choice given prompted options. Two are used to avoid overlap.
+	   flCi is the conversion constant from fluid ounces to cubic inches.
+	 */
+	int finalSay;
+	float vol, outputDim, convertDim, customRadius, cylinderHeight;
+	int inputChoice, inputChoice2;
+	const float pi = 3.1415925635897;
+	const float flCi = 1.80469;
+
+	cout << "\n\n\nWelcome. Enter a liquid amount, in fluid ounces, and this program will determine the packaging dimensions needed to contain that fluid."
+		<< " This program uses inches and fluid ounces."  
+		<< " You will get to choose between three package types: cubic, cylindric and spheric." 
+		<< " Please follow the instructions that follow.\n\n";
+
+	cout << "Enter the amount of fluid, in fluid ounces, that you need to package. Enter a number only:\n\n>>>";
+	cin >> vol;
+	if (cin)
+	{
+		convertDim = vol * flCi;
+		cin.ignore();
+		cout << "\nNow, choose a package type. Enter a corresponding number:\n" 
+			<< "Enter [1] for a cube type.\n"
+			<< "Enter [2] for a cylinder type.\n"
+			<< "Enter [3] for a sphere type.\n"
+			<< "Enter anything else to quit.\n\n>>>";
+
+		cin >> inputChoice;
+		cin.ignore();
+		switch (inputChoice)
+		{
+			case 1: 
+				cout << "\nCube Type with " << vol << " fl oz.";
+				outputDim = cbrtf(convertDim);
+				cout << "\n\nFinal dimensions: " << outputDim << " inches on each length.\n";
+				break;
+			case 2:
+				cout << "\nNow, choose your type of cylinder. "
+					<< "'Regular' cylinders are twice as tall as they are wide; height is twice diameter. "
+					<< "'Custom' cylinders are those which you enter a radius, and height will be calculated\n"
+					<< "Enter [1] for a regular cylinder.\n"
+					<< "Enter [2] for a custom cylinder.\n"
+					<< "Enter anything else to quit.\n\n>>>";
+
+				cin >> inputChoice2;
+				cin.ignore();
+				switch (inputChoice2)
+				{
+					case 1:
+						cout << "\nRegular cylinder with " << vol << " fl oz.";
+						outputDim = cbrtf((convertDim / (4.0*pi)));
+						cylinderHeight = outputDim * 4.0;
+						cout << "\n\nFinal dimensions: " << outputDim << " inch radius with a " << cylinderHeight << " inch height.\n";		
+						break;
+					case 2:
+						cout << "\nEnter a radius.\n\n>>>";
+						cin >> customRadius;
+						cin.ignore();
+						cout << "\nCusom cylinder with " << vol << " fl oz and a " << customRadius << " inch radius.";
+						cylinderHeight = convertDim / (pi * powf(customRadius, 2.0));
+						cout << "\n\nFinal dimensions: " << customRadius << " inch radius with a " << cylinderHeight << " inch height.\n";
+						break;
+					default:
+						exit(0);
+						break;
+				}
+				break;
+			case 3: 
+				cout << "\nSphere Type with "<< vol << " fl oz.";
+				outputDim = cbrtf((.75*convertDim));
+				cout << "\n\nFinal dimensions: " << outputDim << " inch radius for the sphere.\n";
+				break;
+			default:
+				exit(0);
+				break;
+		}
+	}
+	else
+	{
+		cout << "There was a problem with your input." << endl;
+		cin.clear();
+		cin.ignore();
+	}
+
+	cout << "\nDo another round? Enter[1] to continue, otherwise the program will quit.\n\n>>>";
+	cin >> finalSay;
+	return finalSay;		
+}
+
+
 
 int main()
 {
+	bool keepGoing = true;
+	while (keepGoing)
+	{
+		int runner();
+		if (runner() != 1)
+		{
+			keepGoing = false;
+		}
+	}		
 	return 0;
 }
